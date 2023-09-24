7ec59deadc144478a5961f92506ebb7e29fa3fa4
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-24 12:33:11 +0530

    program_____*****         ____*****         ___*****         __*****         _*****

---
+++ Patterm-programs/src/Seven.java
@@ -0,0 +1,27 @@
+//Print
+//        _____*****
+//        ____*****
+//        ___*****
+//        __*****
+//        _*****
+
+
+public class Seven {
+    public static void main(String[] args) {
+
+        for(int i= 5; i>=1;i--){
+
+            for (int j=1; j<=i; j++){
+                System.out.print("_");
+
+            }
+            for(int k=1; k<=5 ;k++){
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+
+        }
+    }
+}
--- Patterm-programs/src/five.java
+++ Patterm-programs/src/five.java
@@ -7,11 +7,20 @@
 //*
 
 
+import java.util.Scanner;
+
 public class five {
     public static void main(String[] args) {
 
 
-        for(int i =1 ; i<=6; i++){
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("enter the number");
+        int num = obj.nextInt();
+        System.out.println( num);
+
+
+        for(int i =1 ; i<=num; i++){
 //
             for(int j =1; j<=i;j++){
                 System.out.print("*");
