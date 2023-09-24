2de809dabd5895880366cbfe939369340013981f
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-24 10:05:54 +0530

    program *          **          ***          ****          *****

--- Patterm-programs/src/Third.java
+++ Patterm-programs/src/Third.java
@@ -1,2 +1,23 @@
+// Print ******
+//       ******
+//       ******
+//       ******
+
+
 public class Third {
+    public static void main(String []args){
+
+
+
+        for( int i =1; i<=4; i++){
+
+            for (int j =1;j<=4;j++){
+                System.out.print("*");
+            }
+
+
+            System.out.println();
+        }
+
+    }
 }
