fd488a0aa02109ad88478df7f4415c989857f20c
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-25 14:45:42 +0530

    We can use static in place of public and Write any thing in place of args              **

---
+++ Patterm-programs/src/Twelve.java
@@ -0,0 +1,7 @@
+//We can use static in place of public and Write any thing in place of args
+public class Twelve {
+
+    static public void main(String[] arnshul){
+        System.out.print("anshul");
+    }
+}
