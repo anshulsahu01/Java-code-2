d717e8bc0274aa23d68568c33eca0123c8ab5f85
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-28 15:16:02 +0530

    Lower data type to higher

---
+++ TypeCasting/src/Explicit.java
@@ -0,0 +1,10 @@
+public class Explicit {
+    public static void main(String[] args) {
+        double a = 124.45;
+        byte b;
+        b =(byte) a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
