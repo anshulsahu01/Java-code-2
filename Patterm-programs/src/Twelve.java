c269e62e975d1825bba6832de4f52745169bf81c
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-28 22:06:52 +0530

    Print
    *****
    *   *
    *   *
    *   *
    *****

--- Patterm-programs/src/Twelve.java
+++ Patterm-programs/src/Twelve.java
@@ -1,7 +1,45 @@
+/*
+Print
+
+*****
+*   *
+*   *
+*   *
+*****
+
+ */
+
+import java.util.Scanner;
+
 //We can use static in place of public and Write any thing in place of args
 public class Twelve {
 
     static public void main(String[] arnshul){
-        System.out.print("anshul");
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number of rows you want ");
+
+        int n = obj.nextInt();
+
+
+
+        for(int i=1;i<=n;i++ ){
+
+            for (int j=1 ;j<=n;j++){
+
+                if(i==1||i==n||j==1||j==n){
+                    System.out.print("*");
+
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+
+            } System.out.println();
+
+        }
+
+//        System.out.print("anshul");
     }
 }
