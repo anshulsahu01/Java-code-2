44cef02975a497dca99ca590bf507b895a5461f1
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-04 16:58:19 +0530

    3D arrays taking input from the user

---
+++ Arrys/src/threeDarrays.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class threeDarrays {
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("enter the number of School is there");
+        int school =obj.nextInt();
+        System.out.println("enter the number of class is there in each school");
+        int cls =obj.nextInt();
+
+        System.out.println("enter the number of Student is there in each class");
+        int stud =obj.nextInt();
+
+        int[][][] arr = new int[school][cls][stud];
+
+        for(int i=0;i<=arr.length-1;i++){
+
+            for(int j=0;j<=arr[i].length-1;j++){
+
+                for(int k=0;k<=arr[j].length-1;k++){
+                    System.out.println("enter the marks of schools "+i+" class "+j+" student "+k);
+
+                   arr[i][j][k] =  obj.nextInt();
+                }
+
+            }
+
+        }
+        for(int i=0;i<=arr.length-1;i++){
+
+            for(int j=0;j<=arr[i].length-1;j++){
+
+                for(int k=0;k<=arr[j].length-1;k++){
+                    System.out.println(" the Stored marks of schools "+i+" class "+j+" Student "+k);
+
+                   System.out.println( arr[i][j][k]);
+                }
+
+            }
+
+        }
+
+    }
+}
