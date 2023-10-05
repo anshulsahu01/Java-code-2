c88ccd071ae3eebecddab1a7dbb21d1970c17648
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-05 20:13:30 +0530

    A program

--- Pattern-p2/src/printA.java
+++ Pattern-p2/src/printA.java
@@ -9,13 +9,13 @@ public class printA {
 
         for(int i=1; i<=n; i++) {
             for (int j = 1; j <= i; j++) {
-////                if (i = 1 || i = n / 2 || j = 1 || j = n) {
-//                    System.out.print("*");
-//                } else {
-//                    System.out.print("_");
-//                }
+                if ( j == 1||i == 1|| j == n||i == n / 2 ) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
 
-            }System.out.println(" ");
+            }System.out.println();
 
         }
     }
