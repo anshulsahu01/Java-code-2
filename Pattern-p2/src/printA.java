3a1722fd52200e00e1e84588a2c44215f6c56b57
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-05 19:01:21 +0530

    A pattern program

---
+++ Pattern-p2/src/printA.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class printA {
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter the no. of rows");
+        int n = obj.nextInt();
+
+
+        for(int i=1; i<=n; i++) {
+            for (int j = 1; j <= i; j++) {
+////                if (i = 1 || i = n / 2 || j = 1 || j = n) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print("_");
+//                }
+
+            }System.out.println(" ");
+
+        }
+    }
+
+}
