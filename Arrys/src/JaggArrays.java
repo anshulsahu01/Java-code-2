4628ee25c0538a0d4fd763be904274a753836b68
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-05 19:02:02 +0530

    jagged Arrays program

---
+++ Arrys/src/JaggArrays.java
@@ -0,0 +1,11 @@
+public class JaggArrays {
+    public static void main(String arg[]){
+
+        int[][] a = new int[2][];
+        a[0]=new int[2];
+        a[1]=new int [1];
+        
+
+
+    }
+}
