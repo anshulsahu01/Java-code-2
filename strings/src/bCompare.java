f7a8ad4b414a34ea9b0370f535064eb4064e9440
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-06 14:56:02 +0530

    == String program

---
+++ strings/src/bCompare.java
@@ -0,0 +1,14 @@
+public class bCompare {
+    public static void main(String[] args){
+
+        String a1 ="rama";
+        String a2= "sita";
+
+if(a1==a2){
+System.out.println("equale");
+    }else{
+System.out.println("not equal");
+
+}
+}
+}
