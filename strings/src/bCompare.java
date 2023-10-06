46529f01682e745878984b380aad2b1677ef07ce
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-06 15:02:52 +0530

    equals String program

--- strings/src/bCompare.java
+++ strings/src/bCompare.java
@@ -2,7 +2,7 @@ public class bCompare {
     public static void main(String[] args){
 
         String a1 ="rama";
-        String a2= "sita";
+        String a2="rama";
 
 if(a1==a2){
 System.out.println("equale");
---
+++ strings/src/cEquals.java
@@ -0,0 +1,13 @@
+public class cEquals {
+    public static void main(String args[]){
+        String s1 ="rama";
+        String s2 ="Rama"; //same mtlv same capital small kuch nai same hona chahiye sb
+
+        if(s1.equals(s2)){
+            System.out.println("String values are equal");
+
+        }else{
+            System.out.println("String values are not equal");
+        }
+    }
+}
