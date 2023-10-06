e19f2ed24e62c9d3d45def4834602ca3e87e07e9
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-06 15:30:59 +0530

    a1+a2 == String program

---
+++ strings/src/fCompare.java
@@ -0,0 +1,18 @@
+public class fCompare {
+    public static void main(String[] args){
+
+        String a1 ="rama";
+        String a2="sita";
+        String a3 =a1+a2;
+        String a4 =a1+a2;
+        if(a3==a4){
+            System.out.println("reference is equale");
+        }else{
+            System.out.println("reference is not equal");
+
+        }
+
+
+
+    }
+}
