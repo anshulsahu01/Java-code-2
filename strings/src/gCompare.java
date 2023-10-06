07022484ba56b73d28c434d1f5e6b1d8cd5cadd8
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-06 15:32:53 +0530

    a1+"sita" String program

---
+++ strings/src/gCompare.java
@@ -0,0 +1,18 @@
+public class gCompare {
+    public static void main(String[] args){
+
+        String a1 ="rama";
+        String a2="sita";
+        String a3 =a1+"sita";
+        String a4 ="rama"+a2;
+        if(a3==a4){
+            System.out.println("reference is equale");
+        }else{
+            System.out.println("reference is not equal");
+
+        }
+
+
+
+    }
+}
