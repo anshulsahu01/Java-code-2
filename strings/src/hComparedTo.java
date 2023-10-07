0a5d4b44626d1c676073094451eef17a228a36ed
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-07 14:58:19 +0530

    ascai value

---
+++ strings/src/hComparedTo.java
@@ -0,0 +1,15 @@
+public class hComparedTo {
+    public static void main(String args[]){
+
+
+        char a='U';
+        System.out.println((int)a);
+
+        String s1 ="SAURAB";
+        String s2 ="SACHIN";
+
+//        if(s1.CompareTo(s2)){
+//
+        }
+    }
+
