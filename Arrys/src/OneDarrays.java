ee212cfd5dbd898fb2c5c6dc900cccd871b84ef9
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-07 19:37:56 +0530

    .

--- Arrys/src/OneDarrays.java
+++ Arrys/src/OneDarrays.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class OneDarrays {
     public static void main(String args[]){
 
-//        int[] name = new int[5];
-//
-//
-//        Scanner obj = new Scanner(System.in);
-//        System.out.println("enter the size of arrays");
-//        int x = obj.nextInt();
-//
-//
-////        int[] num   ={1,2,3,4};
-//
-//        int arr[]= new int[x];
-//        System.out.println("know enter the number which you want to store");
-//        for(int i=0; i<=x-1;i++ ){
-//            arr[i] = obj.nextInt();
-//        }
-//        System.out.println("the stored element are");
-//        for(int i=0; i<=x-1;i++){
-//            System.out.println(arr[i]);
-////            System.out.println(arr.length);
-//        }
+        int[] name = new int[5];
+
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter the size of arrays");
+        int x = obj.nextInt();
+
+
+//        int[] num   ={1,2,3,4};
+
+        int arr[]= new int[x];
+        System.out.println("know enter the number which you want to store");
+        for(int i=0; i<=arr.length-1;i++ ){
+            arr[i] = obj.nextInt();
+        }
+        System.out.println("the stored element are");
+        for(int i=0; i<=arr.length;i++){
+            System.out.println(arr[i]);
+//            System.out.println(arr.length);
+        }
 
 
 
