76607d9a3bf9f66b6a1365425c2c7acb175b9108
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-07 18:31:04 +0530

    all the conditions

---
+++ strings/src/iImmutatble.java
@@ -0,0 +1,14 @@
+public class iImmutatble {
+    public static void main(String args[]){
+
+        String s1 = "SITA";
+        System.out.println(s1);
+
+//        s1.conate("ravana");
+
+
+        System.out.println(s1);
+    }
+
+
+}
