ccc639ffe158937acc6a9790e3910b2a60f46d9e
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-07 15:04:10 +0530

    it will - s1 ,s2

--- strings/src/hComparedTo.java
+++ strings/src/hComparedTo.java
@@ -2,14 +2,20 @@ public class hComparedTo {
     public static void main(String args[]){
 
 
-        char a='U';
-        System.out.println((int)a);
+//        char a='U';
+//        System.out.println((int)a);
 
         String s1 ="SAURAB";
         String s2 ="SACHIN";
 
-//        if(s1.CompareTo(s2)){
-//
+        if(s1.compareTo(s2)>0){
+            System.out.println("first string is greater  "  +s1.compareTo(s2));
+        }else if(s1.compareTo(s2)<0){
+            System.out.println("Second  string is greater");
+        }else{
+            System.out.println("string are equal");
+        }
+
         }
     }
 
