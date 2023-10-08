5de0eee7a6a878c69e349742b75c708053c2fffc
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-08 14:36:24 +0530

    Print twoCross  *       *   *     *    *   *     * *      *     * *    *   *   *     *  *       *

--- Pattern-p2/src/fCrossTwo.java
+++ Pattern-p2/src/fCrossTwo.java
@@ -1,3 +1,49 @@
+/*
+ *   **    *
+ * *  *  *
+ *    **
+ * *   **
+ *   * *  *
+ *   *    *
+ * * *  *
+ *   **
+ * *  **
+ *   **  *
+ *    **   *
+
+ */
+
+
+
+import java.util.Scanner;
+
 public class fCrossTwo {
 
-}
+        public static void main(String args[]){
+
+            Scanner obj = new Scanner(System.in);
+            System.out.println("enter the number of rows");
+            int n = obj.nextInt();
+
+            for(int i= 0; i<=n-1;i++){
+                for(int j=0;j<=n-1;j++){
+
+                    if(i==j || i+j==n-1 || i+j==(n/2)-1 || i-j == (n/2)-1 ||j-i == n/2 ||i+j ==n+(n/2)-1){
+                        System.out.print("*");
+
+
+                    }else{
+                        System.out.print(" ");
+                    }
+
+
+                }System.out.println(" ");
+            }
+
+
+        }
+
+    }
+
+
+
