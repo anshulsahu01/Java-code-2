d171b8663000d8e48beda68aaaf379561b5a0c50
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-08 16:54:31 +0530

    6th program with-out user

--- Pattern-p2/NumberPattern/src/eOneZero.java
+++ Pattern-p2/NumberPattern/src/eOneZero.java
@@ -12,17 +12,17 @@ public class eOneZero {
         for(int i=1; i<=4;i++){
             for(int j=1;j<+4;j++){
 
-                if(j%2==0){
-                    System.out.print(" 0 ");
-
-                }else{
-                    System.out.print( " 1 ");
-                }
+//                if(j%2==0){
+//                    System.out.print(" 0 ");
+//
+//                }else{
+//                    System.out.print( " 1 ");
+//                }
 
 
 //                OR output will be same
 
-//                System.out.print(j%2+ "  ");
+                System.out.print(j%2+ "  ");
 
 
 
