ee9d300f2d0ac9e7cf52b37c196e11406fd6aab9
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-08 15:49:16 +0530

    with user

--- Pattern-p2/NumberPattern/src/Main.java
+++ Pattern-p2/NumberPattern/src/Main.java
@@ -1,13 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*1111
+2222
+3333
+4444
+
+ */
+
+import java.util.Scanner;
 public class Main {
     public static void main(String args[]){
+        Scanner obj=new Scanner(System.in);
 
 
-        for(int i=1; i<=4;i++){
-            for(int j=1;j<=4;j++){
+        System.out.println("enter the number of rows's");
+        int n = obj.nextInt();
+
+
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=n;j++){
                 System.out.print(i);
-            }System.out.println("");
+            }System.out.println(" ");
         }
     }
 
