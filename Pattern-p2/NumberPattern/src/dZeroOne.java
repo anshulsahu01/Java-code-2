a7080991373949fef51497e45b45d4cc940ff5b5
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-08 16:49:04 +0530

    5th program with user

---
+++ Pattern-p2/NumberPattern/src/dZeroOne.java
@@ -0,0 +1,13 @@
+/*
+
+ */
+public class dZeroOne {
+    public static void main(String args[]){
+
+        for(int i=1; i<=4;i++){
+            for(int j=1;j<+4;j++){
+                System.out.print(i%2+ "  ");
+            }System.out.println("");
+        }
+    }
+}
