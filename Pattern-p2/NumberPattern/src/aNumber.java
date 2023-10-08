e80a7d4dd30d0b473db4a44f0f06152e787bb013
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-08 15:53:40 +0530

    2nd program with user

---
+++ Pattern-p2/NumberPattern/src/aNumber.java
@@ -0,0 +1,27 @@
+/*
+12345
+12345
+12345
+12345
+12345
+ */
+
+import java.util.Scanner;
+
+public class aNumber {
+    public static void main(String args[]){
+        Scanner obj=new Scanner(System.in);
+
+
+        System.out.println("enter the number of rows's");
+        int n = obj.nextInt();
+
+
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(j);
+            }System.out.println(" ");
+        }
+    }
+
+}
