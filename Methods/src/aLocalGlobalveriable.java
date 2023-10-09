12a376dd1f54ac297652c1db7cabcd3a047d6abc
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-09 20:07:42 +0530

    Local,Global veriable

---
+++ Methods/src/aLocalGlobalveriable.java
@@ -0,0 +1,16 @@
+package Methods.src;
+
+public class aLocalGlobalveriable {
+    public static void main(String[] args){
+        // local varible because it is inside the main
+
+        int a;
+        float b;
+        double c;
+        String d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
