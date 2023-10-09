8d064ec42fed1a467b51889a4e211f7b3ab5e908
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-09 21:19:21 +0530

    No input no-output

---
+++ Methods/src/TypeofMethods.java
@@ -0,0 +1,22 @@
+// No input no-output
+
+package Methods.src;
+
+public class TypeofMethods {
+    public static void main(String[] args){
+        Calculater c = new Calculater();
+        c.add();
+
+    }
+
+
+}
+class Calculater{
+    void add(){
+        int a =10;
+        int b=20;
+        int c = a+b;
+        System.out.println("the sum of both the number is "+c);
+    }
+
+}
