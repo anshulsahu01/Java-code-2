b1e3e49eee7d7fb9c45aab7bb12e7fbecdd88cd0
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-14 14:08:24 +0530

    #11111$ #22222$ #33333$ #44444$ #55555$

---
+++ Pattern-p2/NumberPattern/src/hProgram.java
@@ -0,0 +1,24 @@
+/*
+#11111$
+#22222$
+#33333$
+#44444$
+#55555$
+
+ */
+
+
+public class hProgram {
+    public static void main(String args[]){
+
+        for(int i=1;i<=5;i++){
+            System.out.print("#");
+
+            for(int j=1;j<=5;j++){
+                System.out.print(i);
+
+            }System.out.print("$");
+            System.out.println("");
+        }
+    }
+}
