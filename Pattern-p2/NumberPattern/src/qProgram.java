271946eba494912fee155c696c72ae7446a8a506
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-15 15:16:52 +0530

    data

---
+++ Pattern-p2/NumberPattern/src/qProgram.java
@@ -0,0 +1,18 @@
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+ */
+public class qProgram {
+    public static void main(String[] args){
+
+        int c =1;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(c++ +" ");
+            }System.out.println(" ");
+        }
+
+    }
+}
