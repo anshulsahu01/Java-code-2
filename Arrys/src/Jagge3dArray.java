9fad8b61c6f8fb772c684cef9198afa9687d7e5f
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-20 13:53:19 +0530

    Static

---
+++ Arrys/src/Jagge3dArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Jagge3dArray {
+    public static void main(String[] args){
+
+        Scanner obj = new Scanner(System.in);
+        int school=2 ;
+
+        int cls;
+        int student;
+//        int[][][] arr =new int[school][cls][student];
+        System.out.println("class in school 1");
+
+
+
+
+    }
+
+}
