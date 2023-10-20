08dd6f424ffcbb009cf48d018d14cfdabe804cac
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-20 16:25:47 +0530

    inheritance

---
+++ OOPS/Inheritance/src/Main.java
@@ -0,0 +1,36 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+         friend f1=new friend();
+        f1.disp();
+
+
+
+    }
+}
+
+class you{
+    int aid;
+    int pass;
+    you(){
+        aid=12455;
+        pass=789788;
+
+
+    }
+
+}
+class friend extends you{
+    friend(){
+        aid=7777;
+        pass=8888;
+
+    }
+   void disp(){
+        System.out.println(aid);
+        System.out.println(pass);
+    }
+
+}
