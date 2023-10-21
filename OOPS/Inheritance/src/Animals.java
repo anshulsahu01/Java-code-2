717cdc29937587c6a8a0cc2705ceca19a96d92ae
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-21 20:26:16 +0530

    3 methods in inheritance

---
+++ OOPS/Inheritance/src/Animals.java
@@ -0,0 +1,56 @@
+
+class animal{
+    void eat(){
+        System.out.println(" eats every day");
+    }
+    void sleep(){
+        System.out.println("sleep at night ");
+    }
+    void breath(){
+        System.out.println("breathe ");
+    }
+
+}
+
+class tiger extends animal{
+    void eat(){
+        System.out.println("tiger eats meat");
+    }
+}
+class deer extends animal{
+    void eat(){
+        System.out.println("deer eats grasses,sedges ");
+    }
+}
+class monkey extends animal{
+    void eat(){
+        System.out.println("monkey eats fruits");
+    }
+}
+
+public class Animals {
+
+    public static void main(String args[]){
+
+        monkey m= new monkey();
+        deer d=new deer();
+        tiger t= new tiger();
+
+        t.eat();
+        t.breath();
+        t.sleep();
+        System.out.println("############");
+
+        d.eat();
+        d.breath();
+        d.sleep();
+        System.out.println("@@@@@@@@@@@@@");
+        m.eat();
+        m.breath();
+        m.sleep();
+        System.out.println("///////////");
+
+
+    }
+
+}
