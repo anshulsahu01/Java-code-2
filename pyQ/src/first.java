894f3510d9183aed822076004cceda8ae47882fd
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-01 16:46:39 +0530

    01

---
+++ pyQ/src/first.java
@@ -0,0 +1,13 @@
+public class first {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5};
+//        System.out.println(arr[2]);
+        int sum =0;
+        for(int i=0;i<=arr.length-1;i++){
+            sum =arr[i]+sum;
+
+
+        }System.out.println(sum);
+    }
+}
