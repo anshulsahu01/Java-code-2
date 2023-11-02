ee4eba3ad05d4b345909a126d04121c092115bf8
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-02 16:12:03 +0530

    02

---
+++ OOPS/Polymorphism/src/VersionTwo.java
@@ -0,0 +1,39 @@
+class animal{
+    void eat(){
+        System.out.println(" eats every day");
+    }
+    void sleep(){
+        System.out.println("sleep at night ");
+    }
+    void breath(){
+        System.out.println("breathe ");
+    }
+
+}
+
+class tiger extends animal{
+    void eat(){
+        System.out.println("tiger eats meat");
+    }
+}
+class deer extends animal{
+    void eat(){
+        System.out.println("deer eats grasses,sedges ");
+    }
+}
+class monkey extends animal{
+    void eat(){
+        System.out.println("monkey eats fruits");
+    }
+}
+
+
+
+
+
+
+public class VersionTwo {
+    public static void main(String args[]){
+
+    }
+}
