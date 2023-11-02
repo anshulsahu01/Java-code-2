2f83a37196038b7fd236de952ba79b9d1eaca071
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-02 16:26:17 +0530

    abstraction

---
+++ OOPS/Abstraction/src/Final.java
@@ -0,0 +1,16 @@
+// final in class
+
+
+
+//abstract class plan{
+//    final abstract void start();
+//    abstract void fly();
+//    abstract void land();
+//
+//error
+//}
+
+
+
+public class Final {
+}
---
+++ OOPS/Abstraction/src/Main.java
@@ -0,0 +1,19 @@
+//abstraction 01 code
+
+abstract class Animal{
+    abstract void   eat();
+}
+class Dog extends Animal{
+    void eat(){
+        System.out.println("biscut");
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog d1 =new Dog();
+        d1.eat();
+
+        System.out.printf("good");
+    }
+}
