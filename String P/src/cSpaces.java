4c0cb33d86c7d8e609822dd0b617234b6ff8c553
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-12 14:49:10 +0530

    04

--- String P/src/cSpaces.java
+++ String P/src/cSpaces.java
@@ -4,7 +4,9 @@
 import java.util.Scanner;
 public class cSpaces {
 
+    /*
     public static void main(String args[]){
+
         Scanner obj=new Scanner(System.in);
         String s1=obj.nextLine();
 
@@ -12,6 +14,23 @@ public class cSpaces {
         for(int i=0;i<=s1.length()-1;i++){
 
             if (s1.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println(count+1);
+
+        */
+
+
+    //  this is    my class
+ public static void main(String args[]){
+        Scanner obj=new Scanner(System.in);
+        String s1=obj.nextLine();
+
+        int count =0;
+        for(int i=0;i<=s1.length()-1;i++){
+
+            if (s1.charAt(i) == ' '&& s1.charAt(i)+1!=' ') {
                 count++;
             }
         }
