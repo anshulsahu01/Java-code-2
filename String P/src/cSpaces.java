c6629a10c95e754f8e217febf2db4e0e9095b8f9
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-12 14:31:54 +0530

    04

---
+++ String P/src/cSpaces.java
@@ -0,0 +1,21 @@
+//this is my class
+//find the no. of words in this
+
+import java.util.Scanner;
+public class cSpaces {
+
+    public static void main(String args[]){
+        Scanner obj=new Scanner(System.in);
+        String s1=obj.nextLine();
+
+        int count =0;
+        for(int i=0;i<=s1.length()-1;i++){
+
+            if (s1.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println(count+1);
+
+    }
+}
