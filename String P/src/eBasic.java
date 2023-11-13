152b665b95741b51e204bae7aa772806651a5a4d
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-13 11:29:58 +0530

    05

---
+++ String P/src/eBasic.java
@@ -0,0 +1,13 @@
+/*
+asncy code
+ */
+import java.util.Scanner;
+public class eBasic {
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter a value");
+        char a=obj.next().charAt(0);
+        System.out.println("the asncy code  is   "+(int)a);
+
+    }
+}
