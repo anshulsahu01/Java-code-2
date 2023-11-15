12d254a5196a049e3b3020e032795697b2689e58
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-15 08:30:17 +0530

    07

---
+++ String P/src/gConvert.java
@@ -0,0 +1,28 @@
+/*
+converting every string in capital letter
+ */
+import java.util.Scanner;
+public class gConvert {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter a  string");
+        String s=obj.nextLine();
+        String result =" ";
+
+        for(int i=0;i<=s.length()-1;i++){
+            if(s.charAt(i)>=97&&s.charAt(i)<122){
+                result=result+ (char)(s.charAt(i)-32);
+            }else{
+                result=result+s.charAt(i);
+            }
+
+
+
+
+
+        }
+
+        System.out.println(result);
+
+    }
+}
