98d323390ccbe6cf8196f2a3289d1d4220128142
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-11-30 16:57:40 +0530

    02

--- Collection/src/com/anshul/first.java
+++ Collection/src/com/anshul/first.java
@@ -1,4 +1,23 @@
 package com.anshul;
 
+import java.util.LinkedList;
 public class first {
+    public static void main(String args[]){
+
+        LinkedList l1=new LinkedList();
+        l1.add(10);
+        l1.add(20);
+        l1.add(30);
+        l1.add(40);
+        l1.add(50);
+        l1.addFirst(5);
+        l1.addLast(1000);
+
+        System.out.println(l1);
+        l1.add(2,120);
+        System.out.println(l1);
+
+
+
+    }
 }
