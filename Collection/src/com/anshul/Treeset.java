6553d9afbbcd8925489a19d48d7f658d13d4ee1f
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-12-03 17:34:26 +0530

    0

---
+++ Collection/src/com/anshul/Treeset.java
@@ -0,0 +1,14 @@
+package com.anshul;
+import java.util.TreeSet;
+public class Treeset {
+    public static void main(String args[]){
+        TreeSet TS=new TreeSet();
+        TS.add(10);
+        TS.add(30);
+        TS.add(20);
+        TS.add(11);
+        TS.add(15);
+        TS.add(100);
+        System.out.println(TS);
+    }
+}
