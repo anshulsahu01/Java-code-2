23a6ef7e9745d5521e7024fed063e5401c5a22a4
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-12-04 22:25:35 +0530

    03

---
+++ Collection/src/com/anshul/HashSet.java
@@ -0,0 +1,17 @@
+package com.anshul;
+import java.util.TreeSet;
+
+import java.util.TreeSet;
+
+public class HashSet {
+    public static void main(String args[]){
+        HashSet TS=new HashSet();
+//        TS.add(10);
+//        TS.add(30);
+//        TS.add(20);
+//        TS.add(11);
+//        TS.add(15);
+//        TS.add(100);
+        System.out.println(TS);
+    }
+}
