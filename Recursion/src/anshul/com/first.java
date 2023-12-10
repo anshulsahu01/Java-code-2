aa393aa193e5128d7b40cbe6a8d6eb2920a0fead
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-12-10 23:26:08 +0530

    01

---
+++ Recursion/src/anshul/com/first.java
@@ -0,0 +1,24 @@
+package anshul.com;
+
+public class first {
+    public static void main(String arg[]){
+
+        msg();
+    }
+    static void msg(){
+        System.out.println("hey anshul");
+        msg1();
+
+    }  static void msg1(){
+        System.out.println("hey anshul 1 ");
+        msg2();
+    }  static void msg2(){
+        System.out.println("hey anshul 2 ");
+        msg3();
+    }  static void msg3(){
+        System.out.println("hey anshul 3 ");
+        msg4();
+    }  static void msg4(){
+        System.out.println("hey anshul 4 ");
+    }
+}
