cc1b143272136daa761b62415c2d2fa762002157
anshulsahu01 <anshul001sahu@gmail.com>
2024-01-20 22:21:47 +0530

    0

---
+++ DSA/src/aIsPrime.java
@@ -0,0 +1,18 @@
+//for single number
+public class aIsPrime {
+    public static void main(String[] args){
+        int n=7;
+        int temp =0;
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                temp=temp+1;
+            }
+        }
+        if(temp>0){
+            System.out.println(n + "  false");
+        }
+        else {
+            System.out.println("true");
+        }
+    }
+}
