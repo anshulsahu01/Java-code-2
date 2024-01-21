cdd47376817879a1f43c0b0d7734e165593653f5
anshulsahu01 <anshul001sahu@gmail.com>
2024-01-21 22:29:09 +0530

    0

--- DSA/src/aIsPrime.java
+++ DSA/src/aIsPrime.java
@@ -1,18 +1,73 @@
+import java.util.Scanner;
+
 //for single number
 public class aIsPrime {
     public static void main(String[] args){
-        int n=7;
-        int temp =0;
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
-                temp=temp+1;
+//        int n=7;
+//        int temp =0;
+//
+//        for(int i=2;i<=n-1;i++){
+//            if(n%i==0){
+//                temp=temp+1;
+//            }
+//        }
+//        if(temp>0){
+//            System.out.println(n + "  false");
+//        }
+//        else {
+//            System.out.println("true");
+//        }
+
+//        int n=20;
+//        for(int j=1;j<=n;j++) {
+//
+//            int temp = 0;
+//
+//
+//            for (int i = 2; i <= j- 1; i++) {
+//
+//                if(j%i==0){
+//                    temp=temp+1;
+//                }
+//            }if(temp==0){
+//                System.out.println(j);
+//            }else{
+//                temp=0;
+//            }
+//
+//        }
+
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=obj.nextInt();
+/*
+
+
+        for(int i =1;i<=n;i++){
+            int temp =0;
+            for(int j=2;j<=i-1;j++){
+                if(i%j==0){
+                    temp=temp+1;
+                }
+            } if (temp==0) {
+                System.out.println(i+"t");
+
+            } else{
+                System.out.println(i+"f");
+                temp=0;
             }
+*/
+
         }
-        if(temp>0){
-            System.out.println(n + "  false");
-        }
-        else {
-            System.out.println("true");
-        }
+
+
+
+
+
+
+
+
+
+
     }
 }
