dc0da2cd3857043f929363f7712edc1f42544ff0
anshulsahu01 <anshul001sahu>
2024-04-01 21:02:39 +0530

    1

---
+++ DSA/KK/src/Recursion/SumOfDigit.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        int ans=digtiadd(505);
+        System.out.println(ans);
+
+    }
+
+    public static int digtiadd(int n){
+
+        if(n%10==n){
+            return n;
+        }
+        return (n%10)+digtiadd(n/10);
+    }
+}
