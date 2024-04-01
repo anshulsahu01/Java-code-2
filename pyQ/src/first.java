406e000f2df010244dc75d6a0813cd51ea0946fb
anshulsahu01 <anshul001sahu>
2024-04-01 21:02:19 +0530

    1

--- pyQ/src/first.java
+++ pyQ/src/first.java
@@ -40,6 +40,8 @@ public class first {
 
 
 
+
+
 //        String s1 ="abcabc";//output :- abc
 //        String res=" ";
 //
@@ -53,20 +55,134 @@ public class first {
 //            }
 //        }System.out.println(res);
 
+//        String a="aba";
+//
+//        String tem= " ";
+//        for(int i=a.length()-1;i>=0;i--){
+//
+//            tem=tem+a.charAt(i);
+//            System.out.println(tem);
+//
+//        }
+//        if(a.equals(tem)){
+//            System.out.println("true");
+//        }else {
+//            System.out.println("false");
+//        }
+//        System.out.println(tem);
+
+
+//        String a ="ABCABC";
+//        String b ="AC";
+//
+//        String res="";
+//        for(int i=0;i<=a.length()-1;i++){
+//            for(int j=0;j<=b.length()-1;j++){
+//                if(a.charAt(i)==b.charAt(j) ){
+//                    res=res+a.charAt(i);
+//                }
+//            }
+//        }System.out.println(res);public class CharacterCounter {
+//    public static void main(String[] args) {
+//        String input = "r$3y@o9hy#*a&r"; // Replace this with user input if needed
+//
+//        int digitCount = 0;
+//        int alphabetCount = 0;
+//        int specialCharCount = 0;
+//
+//        for (int i = 0; i < input.length(); i++) {
+//            char ch = input.charAt(i);
+//            if (ch >= '0' && ch <= '9') {
+//                digitCount++;
+//            } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+//                alphabetCount++;
+//            } else {
+//                specialCharCount++;
+//            }
+//        }
+//
+//        System.out.println("Numbers: " + digitCount);
+//        System.out.println("Alphabets: " + alphabetCount);
+//        System.out.println("Special Characters: " + specialCharCount);
+//    }
+//}
 
 
 
-        String a ="ABCABC";
-        String b ="AC";
+//        public class CharacterCounter {
+//            public static void main(String[] args) {
+//                String input = "r$3y@o9hy#*a&r"; // Replace this with user input if needed
+//
+//                int digitCount = 0;
+//                int alphabetCount = 0;
+//                int specialCharCount = 0;
+//
+//                for (int i = 0; i < input.length(); i++) {
+//                    char ch = input.charAt(i);
+//                    if (ch >= '0' && ch <= '9') {
+//                        digitCount++;
+//                    } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+//                        alphabetCount++;
+//                    } else {
+//                        specialCharCount++;
+//                    }
+//                }
+//
+//                System.out.println("Numbers: " + digitCount);
+//                System.out.println("Alphabets: " + alphabetCount);
+//                System.out.println("Special Characters: " + specialCharCount);
+//            }
+//        }
 
-        String res="";
-        for(int i=0;i<=a.length()-1;i++){
-            for(int j=0;j<=b.length()-1;j++){
-                if(a.charAt(i)==b.charAt(j) ){
-                    res=res+a.charAt(i);
+
+        //Pattern q
+//        for(int i=1;i<5;i++){
+//
+//            for(int j=0;j<i;j++){
+//                System.out.print(i);
+//            }
+//            System.out.println("");
+//        }
+
+
+//Q2
+
+//                int n = 4; // Change this value to adjust the size of the pattern
+
+                // Printing upper half of the pattern
+//                for (int i = n; i >= 1; i--) {
+//                    for (int j = 1; j <= i; j++) {
+//                        System.out.print(n - i + 1);
+//                    }
+//                    System.out.println();
+//                }
+//
+//                // Printing lower half of the pattern
+//                for (int i = 2; i <= n; i++) {
+//                    for (int j = 1; j <= i; j++) {
+//                        System.out.print(n - i + 1);
+//                    }
+//                    System.out.println();
+//                }
+
+                //APPLE no of repetaion
+
+        String s1="APPLE";
+
+        for(int i=0;i<s1.length();i++) {
+            int temp = 1;
+            for(int j=i+1;j<s1.length()-1;j++){
+
+                if(s1.charAt(i)==s1.charAt(j)){
+                    temp++;
+//                    s1.[j]=;
+
                 }
-            }
-        }System.out.println(res);
+
+            }System.out.print(s1.charAt(i)+""+temp);
+
+
+        }
 
     }
 }
