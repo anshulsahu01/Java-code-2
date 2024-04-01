022428f98e641c47bcc4b9e4f493c88a435d046f
anshulsahu01 <anshul001sahu>
2024-04-01 21:02:03 +0530

    1

---
+++ DSA/KK/src/Recursion/Fact.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class Fact {
+    public static void main(String[] args) {
+//        int ans=fact(15);
+        int ans =sum(10);
+        System.out.println(ans);
+
+    }
+    public static int fact(int n){
+
+        if(n<=1){
+            return 1;
+        }
+        return n* fact(n-1);
+    }
+
+    public static int sum(int n){
+        if(n<=1){
+            return 1;
+        }
+
+        return n+sum(n-1);
+    }
+}
