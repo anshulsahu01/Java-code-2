2440bcd6c0f466da60bb35cca9f665c865196504
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2024-06-11 21:06:34 +0530

    Update Nine.java

--- Patterm-programs/src/Nine.java
+++ Patterm-programs/src/Nine.java
@@ -1,12 +1,24 @@
+
+/*
+     *
+    **
+   ***
+  ****
+ *****
+
+  
+  */
+
+
 public class Nine {
     public static void main(String[] args) {
         for(int i=1; i<=5;i++){
 
-            for(int j=5; j>=i; i--){
+            for(int j=5; j>=i; j--){
                 System.out.print(" ");
 
             }
-            for (int k=1; k>=i;k++){
+            for (int k=1; k<=i;k++){
                 System.out.print("*");
             }
             System.out.println();
